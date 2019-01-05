e9a1b54a18ea5c0ae330361434017cda995946f5
user <rane@ranewallin.com>
2019-01-05 16:47:12 -0700

    styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+import "../css/Header.css";
 import React from "react";
 
 const Header = props => {
-  return <div>Header</div>;
+  return (
+    <div className="app-header">
+      <div className="header-container">Header</div>
+    </div>
+  );
 };
 
 export default Header;
