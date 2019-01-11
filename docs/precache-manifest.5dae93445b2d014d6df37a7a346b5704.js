0cdd8a8a82f6f5de29ff2053ffe5b792cf286f28
Rane Wallin <RAN2143066@maricopa.edu>
2019-01-11 12:13:22 -0700

    setting up build directory and docs

---
+++ docs/precache-manifest.5dae93445b2d014d6df37a7a346b5704.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    revision: "ec9ca7f753a1580128775e008f7cadd8",
+    url: "/static/media/thumbnail.ec9ca7f7.png"
+  },
+  {
+    revision: "9ba73ec56b5297243c42259ac6f04f05",
+    url: "/static/media/pink_header.9ba73ec5.jpg"
+  },
+  {
+    revision: "229c360febb4351a89df",
+    url: "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    revision: "0912cf15d9647b85db7a",
+    url: "/static/js/main.0912cf15.chunk.js"
+  },
+  {
+    revision: "4a34e45d34cafb7623bd",
+    url: "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    revision: "0912cf15d9647b85db7a",
+    url: "/static/css/main.0ff77836.chunk.css"
+  },
+  {
+    revision: "595c9ad3c6dc8f9c5c387b9f3a3b594c",
+    url: "/index.html"
+  }
+];
---
+++ docs/static/js/main.0912cf15.chunk.js
@@ -0,0 +1,193 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  [
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    ,
+    function(e, a, t) {
+      e.exports = t(23);
+    },
+    ,
+    ,
+    ,
+    ,
+    ,
+    function(e, a, t) {},
+    ,
+    function(e, a, t) {},
+    ,
+    function(e, a, t) {},
+    ,
+    function(e, a, t) {},
+    ,
+    function(e, a, t) {
+      e.exports = t.p + "static/media/thumbnail.ec9ca7f7.png";
+    },
+    function(e, a, t) {
+      "use strict";
+      t.r(a);
+      var n = t(0),
+        c = t.n(n),
+        l = t(2),
+        r = t.n(l),
+        m = t(3),
+        i = t(4),
+        s = t(6),
+        u = t(5),
+        o = t(7),
+        d = (t(14),
+        function(e) {
+          return c.a.createElement(
+            "div",
+            { className: "app-header" },
+            c.a.createElement(
+              "div",
+              { className: "header-container" },
+              "Header"
+            )
+          );
+        }),
+        E = (t(16),
+        function(e) {
+          return c.a.createElement(
+            "div",
+            { className: "tc-container" },
+            "TagCloud"
+          );
+        }),
+        v = (t(18),
+        t(20),
+        function(e) {
+          return c.a.createElement(
+            "div",
+            { className: "ui card" },
+            c.a.createElement(
+              "div",
+              { className: "image" },
+              c.a.createElement("img", { src: t(22), alt: "thumbnail" })
+            ),
+            c.a.createElement(
+              "div",
+              { className: "content" },
+              c.a.createElement(
+                "a",
+                { href: "index.html", className: "header" },
+                "Project Name"
+              ),
+              c.a.createElement(
+                "div",
+                { className: "meta" },
+                c.a.createElement("span", null, "tag")
+              ),
+              c.a.createElement(
+                "div",
+                { className: "description" },
+                "This is a project card"
+              )
+            )
+          );
+        }),
+        N = function(e) {
+          return c.a.createElement(
+            "div",
+            { className: "p-container" },
+            c.a.createElement(
+              "div",
+              { className: "ui three column grid divided container" },
+              c.a.createElement(
+                "div",
+                { className: "row" },
+                c.a.createElement(
+                  "div",
+                  { className: "column" },
+                  c.a.createElement(v, null)
+                ),
+                c.a.createElement(
+                  "div",
+                  { className: "column" },
+                  c.a.createElement(v, null)
+                ),
+                c.a.createElement(
+                  "div",
+                  { className: "column" },
+                  c.a.createElement(v, null)
+                )
+              ),
+              c.a.createElement(
+                "div",
+                { className: "row" },
+                c.a.createElement(
+                  "div",
+                  { className: "column" },
+                  c.a.createElement(v, null)
+                ),
+                c.a.createElement(
+                  "div",
+                  { className: "column" },
+                  c.a.createElement(v, null)
+                ),
+                c.a.createElement(
+                  "div",
+                  { className: "column" },
+                  c.a.createElement(v, null)
+                )
+              ),
+              c.a.createElement(
+                "div",
+                { className: "row" },
+                c.a.createElement(
+                  "div",
+                  { className: "column" },
+                  c.a.createElement(v, null)
+                ),
+                c.a.createElement(
+                  "div",
+                  { className: "column" },
+                  c.a.createElement(v, null)
+                ),
+                c.a.createElement(
+                  "div",
+                  { className: "column" },
+                  c.a.createElement(v, null)
+                )
+              )
+            )
+          );
+        },
+        p = (function(e) {
+          function a() {
+            return (
+              Object(m.a)(this, a),
+              Object(s.a)(this, Object(u.a)(a).apply(this, arguments))
+            );
+          }
+          return (
+            Object(o.a)(a, e),
+            Object(i.a)(a, [
+              {
+                key: "render",
+                value: function() {
+                  return c.a.createElement(
+                    "div",
+                    null,
+                    c.a.createElement(d, null),
+                    c.a.createElement(E, null),
+                    c.a.createElement(N, null)
+                  );
+                }
+              }
+            ]),
+            a
+          );
+        })(n.Component);
+      r.a.render(c.a.createElement(p, null), document.querySelector("#root"));
+    }
+  ],
+  [[8, 2, 1]]
+]);
+//# sourceMappingURL=main.0912cf15.chunk.js.map
