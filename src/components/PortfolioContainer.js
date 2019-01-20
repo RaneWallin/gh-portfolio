7b38fcaba0068b5a613e6cca614a03d055cccb5a
Rane Wallin <RAN2143066@maricopa.edu>
2019-01-19 20:00:00 -0700

    fixing thumbnail

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,47 +1,34 @@
 import "../styles/PortfolioContainer.scss";
-import React from "react";
+import React, { Component } from "react";
 import PortfolioSnapshot from "./PortfolioSnapshot";
 
-const PortfolioContainer = props => {
-  return (
-    <div className="p-container">
-      <div className="ui three column grid divided container">
-        <div className="row">
-          <div className="column">
-            <PortfolioSnapshot />
-          </div>
-          <div className="column">
-            <PortfolioSnapshot />
-          </div>
-          <div className="column">
-            <PortfolioSnapshot />
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <PortfolioSnapshot />
-          </div>
-          <div className="column">
-            <PortfolioSnapshot />
-          </div>
-          <div className="column">
-            <PortfolioSnapshot />
-          </div>
-        </div>
-        <div className="row">
-          <div className="column">
-            <PortfolioSnapshot />
-          </div>
-          <div className="column">
-            <PortfolioSnapshot />
-          </div>
-          <div className="column">
-            <PortfolioSnapshot />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+class PortfolioContainer extends Component {
+  getData = () => {
+    let xmlhttp = new XMLHttpRequest();
+    let display = null;
+    let projectURL = "../projects/";
+    xmlhttp.open("GET", require(`../projects/projects`), false);
+    xmlhttp.onload = function() {
+      display = xmlhttp.response.split("\n");
+    };
+    xmlhttp.send();
+    console.log(display);
+
+    display.forEach(project => {
+      let projectDisplay = "";
+      let fileLocation = projectURL + project;
+      console.log(fileLocation);
+      xmlhttp.open("GET", require(`../projects/` + project), false);
+      xmlhttp.onload = function() {
+        projectDisplay = xmlhttp.response;
+      };
+      xmlhttp.send();
+      console.log(projectDisplay);
+    });
+  };
+  render() {
+    return <div className="p-container">{this.getData()}</div>;
+  }
+}
 
 export default PortfolioContainer;
--- src/components/PortfolioSnapshot.js
+++ src/components/PortfolioSnapshot.js
@@ -1,23 +1,25 @@
 import "../styles/PortfolioSnapshot.css";
-import React from "react";
+import React, { Component } from "react";
 
-const PortfolioSnapshot = props => {
-  return (
-    <div className="ui card">
-      <div className="image">
-        <img src={require("../images/thumbnail.jpg")} alt="thumbnail" />
-      </div>
-      <div className="content">
-        <a href="index.html" className="header">
-          Project Name
-        </a>
-        <div className="meta">
-          <span>tag</span>
+class PortfolioSnapshot extends Component {
+  render() {
+    return (
+      <div className="ui card">
+        <div className="image">
+          <img src={require("../images/thumbnail.jpg")} alt="thumbnail" />
         </div>
-        <div className="description">This is a project card</div>
+        <div className="content">
+          <a href="index.html" className="header">
+            Project Name
+          </a>
+          <div className="meta">
+            <span>tag</span>
+          </div>
+          <div className="description">This is a project card</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PortfolioSnapshot;
