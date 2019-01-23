eccb6ec8df9425a16458ebb1a4857539321a3a17
Rane Wallin <RAN2143066@maricopa.edu>
2019-01-22 20:55:45 -0700

    setting up react router

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,10 +1,3 @@
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDmCAjaV3t72VBcSzLgV6ZlTGnlXYYaO1s",
-  authDomain: "my-portfolio-2abd0.firebaseapp.com",
-  databaseURL: "https://my-portfolio-2abd0.firebaseio.com",
-  projectId: "my-portfolio-2abd0",
-  storageBucket: "",
-  messagingSenderId: "929903781128"
-};
+var config = {};
 firebase.initializeApp(config);
